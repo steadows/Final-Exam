2e470b4dcf02f9b5382e3afdb3fb4afb384a18b1
Steve Meadows <Steve@hermans-imac.client.hermanmiller.com>
2020-12-16 14:19:27 -0500

    cleaned up lines

--- stringfun.c
+++ stringfun.c
@@ -4,8 +4,6 @@
 void bracket_balance(char *, int); // prototype
 
 int main(void){
-
-
         char *s;
         int nread = EOF + 1;;
         size_t maxn = 10;
@@ -30,38 +28,20 @@ void bracket_balance(char *str, int letters){
         int size = letters;
         char *open_arr = (char *) malloc(sizeof(char)*size);
         char character;
-        
 
         for (int i = 0; i < size-1; i++) {
             character = str[i];
                 *(open_arr+i) = character;
         }
-
         int i = size-2;
-        
         while (i >= 0 )
         {
-                if ( *(open_arr+i) == 'g') {
-                        i--;
-                }
-                else if (*(open_arr+i) == 'v')
-                {
-                        i--;
-                }
-                else if (*(open_arr+i) == 's')
-                {
-                        i--;
-                }
-                else if (*(open_arr+i) == 'u')
-                {
-                        i--;
-                }
-                else {
-                        printf("%c", *(open_arr+i));
-                        i--;
-                }
+                if ( *(open_arr+i) == 'g') {i--;}
+                else if (*(open_arr+i) == 'v') {i--;}
+                else if (*(open_arr+i) == 's') {i--;}
+                else if (*(open_arr+i) == 'u') {i--;}
+                else {printf("%c", *(open_arr+i)); i--;}
         }
         printf("\n");
         free(open_arr);
-        
 }
